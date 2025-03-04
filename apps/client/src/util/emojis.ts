bb9fe53c3288a409dfeae07b73004f9c6c5abf91
h1ddenscript <h1ddenscripto@protonmail.com>
2025-03-04 22:00:55 +0000

    Add existing server emojis.
    Co-authored-by: rxin <rxinn@users.noreply.github.com>

---
+++ apps/client/src/util/emojis.ts
@@ -0,0 +1,7 @@
+export default {
+    "skyteam": "<:logo:1231369745987539035>",
+    "alert": "<:alert:1255679013158916177>",
+    "warning": "<:warning:1204923305895665705>",
+    "success": "<:success:1256020727111614586>",
+    "failure": "<:failure:1286647140898308147>"
+}
