5b1aeb8546e98b5f1b1c44e46734425d5c24536b
h1ddenscript <h1ddenscripto@protonmail.com>
2025-03-06 10:47:40 +0000

    Fix outdated drizzle config.

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-	// @ts-ignore
 	dialect: 'postgresql',
 	schema: './src/schema.ts',
 	out: './drizzle',
-	driver: 'pg',
 	dbCredentials: {
-		connectionString: process.env.DATABASE_URL!,
+		url: process.env.DATABASE_URL!,
 	},
 	verbose: true,
 	strict: true,
